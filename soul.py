1615112142440f08965adba13a8de485530ed58f
Paidx25 <yasirbhat756@gmail.com>
2024-07-30 07:02:12 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '7439896119:AAHUmSFQN1X64QzPA51JtAKch3TcTe8uvw4'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
+FORWARD_CHANNEL_ID = -1002184822573
+CHANNEL_ID = -1002184822573
+error_channel_id = -1002184822573
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
